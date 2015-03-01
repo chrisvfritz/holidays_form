dd0b09c651f0baa7537003ef677a13c5aeb42a5f
Chris Fritz <chrisvfritz@gmail.com>
2015-03-01 12:10:49 -0500

    added a few more features to be even lazier and optimize for working with other people and building on the app we already have

--- Gemfile
+++ Gemfile
@@ -3,4 +3,14 @@ ruby '2.2.0'
 
 gem 'sinatra', '~> 1.4.5'
 gem 'rack', '1.5.2'
+# A better webserver than the default WEBrick.
+gem 'thin'
+# Using `bundle exec rackup`, we the server will automatically reload when it needs to.
+gem 'shotgun'
+# An alternative templating language that doesn't make us type as much as ERB does.
+gem 'slim'
+# Some helpers to make forms work more simply.
+gem 'sinatra-formhelpers-ng'
+# Helps us format dates and times nicely by giving an example.
+gem 'stamp'
 gem 'holidapi'
--- app.rb
+++ app.rb
@@ -1,12 +1,25 @@
+# These two lines set up our environment to ensure that only the gem versions we've
+# specified in our Gemfile will be used. You can find our more at:
+# http://bundler.io/bundler_setup.html
+require 'rubygems'
+require 'bundler/setup'
+
 require 'sinatra/base'
+# An alternative templating language that doesn't make us type as much as ERB does.
+require 'slim'
+# Some helpers to make forms work more simply.
+require 'sinatra/form_helpers'
 require 'holidapi'
 # Date is part of the core Ruby library (i.e. is not part of some other gem), but is
-# not loaded by default. If we want to use it, as we do to use Date::MONTHNAMES, then
+# not loaded by default. If we want to use it, as we do to use Date:'MONTH'NAMES, then
 # we need to require it. Find out more about the date library and what else it allows
 # us to do in the Ruby docs: http://ruby-doc.org/stdlib-2.2.0/libdoc/date/rdoc/Date.html
 require 'date'
+# Helps us format dates and times nicely by giving an example.
+require 'stamp'
 
 class HolidayApp < Sinatra::Base
+  helpers Sinatra::FormHelpers
 
   # Establishes SUPPORTED_COUNTRIES as a constant (that's what the all caps mean). A constant
   # just means we define it once, then never change it. We put this here instead of inside of
@@ -31,9 +44,10 @@ class HolidayApp < Sinatra::Base
     # checking not only if the year has no value (i.e. is "nil"), but also if the year
     # is an empty string, which is a possibility in browsers where the `required` attribute
     # doesn't prevent empty submissions of form elements.
-    params[:country] ||= 'US'
-    params[:year]      = @current_time.year if params[:year].nil? || params[:year].empty?
-    params[:month]   ||= @current_time.month
+    params['holidays']            ||= {}
+    params['holidays']['country'] ||= 'US'
+    params['holidays']['year']      = @current_time.year.to_s if params['holidays']['year'].nil? || params['holidays']['year'].empty?
+    params['holidays']['month']   ||= @current_time.month.to_s
 
     # Just in case the user manages to sneak in some bad data, like an empty year for those browsers
     # that don't yet support HTML5 form validations (http://caniuse.com/#feat=form-validation), we
@@ -44,7 +58,7 @@ class HolidayApp < Sinatra::Base
       # all down to one level. In this case, it ensures that we get a single array of hashes, each
       # hash containing a holiday. Find out more about `flatten` in the Ruby docs:
       # http://ruby-doc.org//core-2.2.0/Array.html#method-i-flatten
-      HolidApi.get( country: params[:country], year: params[:year], month: params[:month] ).flatten
+      HolidApi.get( country: params['holidays']['country'], year: params['holidays']['year'], month: params['holidays']['month'] ).flatten
     rescue HolidApi::BadRequest
       []
     end
@@ -52,11 +66,11 @@ class HolidayApp < Sinatra::Base
     # Sets up how we want to display the current country, year, and month. Each one is assigned
     # to an instance variable (one that starts with `@`), so that they will be available in views
     # and helpers.
-    @selected_country = SUPPORTED_COUNTRIES.find { |country| country[:code] == params[:country] }[:name]
-    @selected_year    = params[:year].to_i
-    @selected_month   = Date::MONTHNAMES[params[:month].to_i]
+    @selected_country = SUPPORTED_COUNTRIES.find { |country| country[:code] == params['holidays']['country'] }[:name]
+    @selected_year    = params['holidays']['year']
+    @selected_month   = Date::MONTHNAMES[ params['holidays']['month'].to_i ]
 
-    erb :holidays
+    slim :'holidays/index'
   end
 
   # Helper methods allow you to pull complicated and/or repeated logic out of your views. Helper
@@ -65,6 +79,18 @@ class HolidayApp < Sinatra::Base
 
     def current_search
       "#{@selected_month} #{@selected_year} in #{@selected_country}"
+    end
+
+    def country_options
+      SUPPORTED_COUNTRIES.map { |country| [ country[:code], country[:name] ] }
+    end
+
+    def month_options
+      (1..12).map { |month| [ month.to_s, Date::MONTHNAMES[month] ] }
+    end
+
+    def format_date(date)
+      Date.parse(date).stamp('March 1st, 2015')
     end
 
   end
