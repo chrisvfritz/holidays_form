692091a956dde1b076b6bdbfebccb230e4bea3f7
Chris Fritz <chrisvfritz@gmail.com>
2015-03-15 12:09:41 -0400

    specified that codeclimate should be allowed by webmock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,11 @@ require_relative '../app'
 HolidayApp::Main.environment = :test
 Bundler.require :default, HolidayApp::Main.environment
 
+WebMock.disable_net_connect!(
+  allow_localhost: true,
+  allow: [ /codeclimate.com/ ]
+)
+
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/cassettes'
   config.hook_into :webmock
